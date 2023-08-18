f43786f081a5efd4138c104ad12ff6baf5b34c0e
Jieun Kim <59919953+ji-silver@users.noreply.github.com>
2023-08-19 02:22:18 +0900

    [unrated] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,12 +1,12 @@
 function solution(a, b, n) {
-    let cola = 0; // 총 받은 콜라의 수
-    let bin = n; // 처음에 가지고 있는 빈 병의 수
+    let sum = 0; // 총 받은 콜라의 수
+    let cola = n; // 처음에 가지고 있는 빈 병의 수
 
-    while (bin >= a) {
-        const col = Math.floor(bin / a) * b; // 받은 콜라 수 계산
-        cola += col; // 받은 콜라 수 누적
-        bin = col + (bin % a); // 총 빈 병의 수 계산
+    while (cola >= a) {
+        let col = Math.floor(cola / a) * b; // 받은 콜라 수 계산
+        sum += col; // 받은 콜라 수 누적
+        cola = col + (cola % a); // 총 빈 병의 수 계산
     }
 
-    return cola;
+    return sum;
 }
