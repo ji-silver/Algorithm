32c218fe0034a4f1b29e916d85d7a783d94f6133
Jieun Kim <59919953+ji-silver@users.noreply.github.com>
2023-08-20 01:01:25 +0900

    [unrated] Title: 추억 점수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -1,5 +1,8 @@
 function solution(name, yearning, photo) {
 
+    // 2차원 배열을 map으로 순회하는데 순회할 때 reduce로 추억점수 누적하기
+    // 추억점수는 photo를 map으로 돌릴 때 name에 포함되는지 여부로
+    // 포함 되면 name 배열에서 index 찾고 없으면 0 반환, 초기값은 0
     return photo.map(x => x.reduce((acc, person) =>
         acc + (name.includes(person) ?
             yearning[name.indexOf(person)] : 0), 0))
